b4dab64675bc534c96971fd6dad35aa55dc83a96
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-01-25 21:58:20 +0100

    Finished secrets implementation

--- MintPlayer.GithubApps/SmeeExtensions.cs
+++ MintPlayer.GithubApps/SmeeExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Smee.IO.Client.Dto;
+﻿using Newtonsoft.Json;
+using Smee.IO.Client.Dto;
 using System.Text.Json;
 
 namespace MintPlayer.GithubApps;
@@ -6,19 +7,10 @@ namespace MintPlayer.GithubApps;
 public static class SmeeExtensions
 {
     /// <summary>Correctly reads the webhook from the smee channel.</summary>
-    public static async Task<string> GetFormattedJsonAsync(this SmeeData data)
+    public static string GetFormattedJson(this SmeeData data)
     {
         // Format JSON correctly
-        var rawBody = data.Body.ToString();
-        using var ms = new MemoryStream();
-        using var writer = new Utf8JsonWriter(ms);
-        JsonDocument.Parse(rawBody!).WriteTo(writer);
-
-        await writer.FlushAsync();
-        ms.Seek(0, SeekOrigin.Begin);
-        using var reader = new StreamReader(ms);
-
-        var jsonFormatted = await reader.ReadToEndAsync();
-        return jsonFormatted;
+        var minified = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(data.Body.ToString()));
+        return minified;
     }
 }
