2b4ec14bb6674e0367ff34e4129ff2b17211946b
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-01-25 20:42:17 +0100

    This works when there's no colons in the titile

--- MintPlayer.GithubApps/SmeeService.cs
+++ MintPlayer.GithubApps/SmeeService.cs
@@ -5,7 +5,6 @@ using Smee.IO.Client;
 using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
-using System.Text.Json;
 
 namespace MintPlayer.GithubApps;
 
@@ -14,13 +13,11 @@ public class SmeeService : IHostedService
     private readonly ISmeeClient smeeClient;
     private readonly IConfiguration configuration;
     private readonly IServiceProvider serviceProvider;
-    private readonly ISignatureService signatureService;
-    public SmeeService(ISmeeClient smeeClient, IConfiguration configuration, IServiceProvider serviceProvider, ISignatureService signatureService)
+    public SmeeService(ISmeeClient smeeClient, IConfiguration configuration, IServiceProvider serviceProvider)
     {
         this.smeeClient = smeeClient;
         this.configuration = configuration;
         this.serviceProvider = serviceProvider;
-        this.signatureService = signatureService;
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
@@ -33,55 +30,70 @@ public class SmeeService : IHostedService
     {
         if (e.Event == SmeeEventType.Message)
         {
-            using var scope = serviceProvider.CreateScope();
-            
-            // Format JSON correctly
-            var jsonFormatted = await e.Data.GetFormattedJsonAsync();
+
+            var rawBody = e.Data.Body.ToString(); //.Replace("\r", string.Empty).Replace("\n", string.Empty);
+
+            var trimmed = string.Join(
+                string.Empty,
+                rawBody.Split(Environment.NewLine)
+                    .Select(l => l.Trim(' ').Replace(": ", ":"))
+            );
+                
             var signatureSha256 = e.Data.Headers["x-hub-signature-256"];
             var secret = configuration["GithubApp:WebhookSecret"];
+            if (!string.IsNullOrEmpty(secret))
+            {
+                var keyBytes = Encoding.UTF8.GetBytes(secret);
+                var bodyBytes = Encoding.UTF8.GetBytes(trimmed);
 
-            var isValid = signatureService.VerifySignature(signatureSha256, secret, jsonFormatted);
-            if (!isValid) return;
+                var hash = HMACSHA256.HashData(keyBytes, bodyBytes);
+                var hashHex = Convert.ToHexString(hash);
+                var expectedHeader = $"sha256={hashHex.ToLower(CultureInfo.InvariantCulture)}";
+                if (signatureSha256.ToString() != expectedHeader)
+                {
+                    return;
+                }
+            }
 
-            //var msg = (JObject)e.Data.Body;
+            var msg = (JObject)e.Data.Body;
 
-            //var action = msg["action"].Value<string>();
-            //if (action == "created")
-            //{
-            //    var accessTokensUrl = msg["installation"]["access_tokens_url"];
-            //    var appId = msg["installation"]["app_id"];
-            //    var installationId = msg["installation"]["id"];
-            //}
+            var action = msg["action"].Value<string>();
+            if (action == "created")
+            {
+                var accessTokensUrl = msg["installation"]["access_tokens_url"];
+                var appId = msg["installation"]["app_id"];
+                var installationId = msg["installation"]["id"];
+            }
+            
+            if (action == "deleted")
+            {
 
-            //if (action == "deleted")
-            //{
+            }
 
-            //}
+            if (action == "opened")
+            {
+                //var accessTokensUrl = msg["installation"]["access_tokens_url"];
+                //var appId = msg["installation"]["app_id"];
+                var installationId = msg["installation"]["id"].Value<long>();
 
-            //if (action == "opened")
-            //{
-            //    //var accessTokensUrl = msg["installation"]["access_tokens_url"];
-            //    //var appId = msg["installation"]["app_id"];
-            //    var installationId = msg["installation"]["id"].Value<long>();
+                var jwt = GetJwt(configuration["GithubApp:AppId"]!, configuration["GithubApp:PrivateKeyPath"]!);
 
-            //    var jwt = GetJwt(configuration["GithubApp:AppId"]!, configuration["GithubApp:PrivateKeyPath"]!);
+                var header = new ProductHeaderValue("Test", "0.0.1");
+                var ghclient = new GitHubClient(header)
+                {
+                    Credentials = new Credentials(jwt, AuthenticationType.Bearer)
+                };
 
-            //    var header = new ProductHeaderValue("Test", "0.0.1");
-            //    var ghclient = new GitHubClient(header)
-            //    {
-            //        Credentials = new Credentials(jwt, AuthenticationType.Bearer)
-            //    };
+                var response = await ghclient.GitHubApps.CreateInstallationToken(installationId);
+                var repoClient = new GitHubClient(header)
+                {
+                    Credentials = new Credentials(response.Token)
+                };
 
-            //    var response = await ghclient.GitHubApps.CreateInstallationToken(installationId);
-            //    var repoClient = new GitHubClient(header)
-            //    {
-            //        Credentials = new Credentials(response.Token)
-            //    };
-
-            //    var repositoryId = msg["repository"]["id"].Value<long>();
-            //    var issueNumber = msg["issue"]["number"].Value<int>();
-            //    await repoClient.Issue.Comment.Create(repositoryId, issueNumber, "Thanks for creating an issue");
-            //}
+                var repositoryId = msg["repository"]["id"].Value<long>();
+                var issueNumber = msg["issue"]["number"].Value<int>();
+                await repoClient.Issue.Comment.Create(repositoryId, issueNumber, "Thanks for creating an issue");
+            }
         }
     }
 
