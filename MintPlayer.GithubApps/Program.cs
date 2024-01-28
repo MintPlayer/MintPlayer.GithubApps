462c59c8d3ecc32c5794e829fda2ff57a4f79693
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-01-28 22:18:17 +0100

    Add logging

--- MintPlayer.GithubApps/Program.cs
+++ MintPlayer.GithubApps/Program.cs
@@ -3,23 +3,33 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using MintPlayer.AspNetCore.BotFramework;
 using MintPlayer.AspNetCore.BotFramework.Extensions;
+using MintPlayer.AspNetCore.LoggerProviders;
 using MintPlayer.GithubApps;
 using Octokit.Webhooks.AspNetCore;
 using Smee.IO.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<BotOptions>(options =>
-{
-    options.AppId = builder.Configuration["GithubApp:AppId"];
-    options.WebhookUrl = builder.Configuration["GithubApp:WebhookUrl"];
-    options.WebhookSecret = builder.Configuration["GithubApp:WebhookSecret"];
-    options.PrivateKeyPath = builder.Configuration["GithubApp:PrivateKeyPath"];
-});
+builder.Services
+    .Configure<BotOptions>(options =>
+    {
+        options.AppId = builder.Configuration["GithubApp:AppId"];
+        options.WebhookUrl = builder.Configuration["GithubApp:WebhookUrl"];
+        options.WebhookSecret = builder.Configuration["GithubApp:WebhookSecret"];
+        options.PrivateKeyPath = builder.Configuration["GithubApp:PrivateKeyPath"];
+    })
+    .Configure<FileLoggerOptions>(options =>
+    {
+        options.FileName = "Logs/Log.txt";
+    });
 
 // Add services to the container.
 //builder.Services.AddScoped<IGithubService, GithubService>();
 builder.AddBotFramework<GithubProcessor>();
+builder.Services.AddLogging(options =>
+{
+    options.AddFileProvider();
+});
 builder.Services.AddHttpLogging(options =>
 {
     options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
